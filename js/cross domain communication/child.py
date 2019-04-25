63f71874efd55ad30c1ec703227feae68821ffd0
DmitriyValetov <dmitriy.valetov@phystech.edu>
2019-04-25 12:37:37 +0300

    Added backward communicating from iframe to parent. it has parent global variable... euyyy

--- js/cross domain communication/child.py
+++ js/cross domain communication/child.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('slave_index.html')
+    return render_template('child_index.html')
  
 
 if __name__ == "__main__":
--- js/cross domain communication/parent.py
+++ js/cross domain communication/parent.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('master_index.html')
+    return render_template('parent_index.html')
  
 
 if __name__ == "__main__":
