3ddf11ccfa04bc9b38f5d375d0225cacb7ef4a6c
Dmitriy Valetov <dmitriy.valetov@phystech.edu>
2019-06-17 18:49:15 +0300

    added check freeness

--- python/search_open_local_port/search_port.py
+++ python/search_open_local_port/search_port.py
@@ -9,6 +9,13 @@ def find_open_ports():
             if res == 0:
                 yield port
 
+def port_free(port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        res = sock.connect_ex(('localhost', port))
+        if res == 0:
+            return True
+    return False
+
 
 if __name__ == "__main__":
     port_generator = find_open_ports() 
